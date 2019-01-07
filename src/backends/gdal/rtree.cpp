8fb3fc309c92485337a1eaa826101c36581ade49
James W. McClain <jmcclain@daystrom-data-concepts.com>
2019-01-06 19:58:39 -0500

    Stubs

--- src/backends/gdal/rtree.cpp
+++ src/backends/gdal/rtree.cpp
@@ -24,6 +24,8 @@
 
 #include <cstdio>
 #include <cstdint>
+#include <cstring>
+#include <vector>
 
 // https://www.boost.org/doc/libs/1_69_0/libs/geometry/doc/html/geometry/spatial_indexes/rtree_examples/index_stored_in_mapped_file_using_boost_interprocess.html
 // https://www.boost.org/doc/libs/1_69_0/libs/geometry/doc/html/geometry/spatial_indexes/rtree_examples.html
@@ -45,4 +47,42 @@ typedef bgi::linear<16, 4> params_t;
 typedef bgi::indexable<value_t> indexable_t;
 typedef bgi::rtree<value_t, params_t, indexable_t> rtree_t;
 
-rtree_t rtree();
+
+rtree_t * rtree_ptr = nullptr;
+
+
+extern "C" int init()
+{
+    if (rtree_ptr == nullptr)
+        rtree_ptr = new rtree_t();
+    return 1;
+}
+
+extern "C" int deinit()
+{
+    if (rtree_ptr != nullptr)
+        delete rtree_ptr;
+    rtree_ptr = nullptr;
+    return 1;
+}
+
+extern "C" int insert(const char *filename, uint64_t start, uint64_t end)
+{
+    auto range = range_t(point_t(start), point_t(end));
+    auto filename_str = std::string(strdup(filename));
+    auto value = std::make_pair(range, filename_str);
+
+    rtree_ptr->insert(value);
+    return 1;
+}
+
+extern "C" int query(char **filenames, uint64_t start, uint64_t end)
+{
+  auto range = range_t(point_t(start), point_t(end));
+  auto intersects = bgi::intersects(range);
+  auto candidates = std::vector<value_t>();
+
+  rtree_ptr->query(intersects, std::back_inserter(candidates));
+
+  return 1;
+}
